6bfcac8c28181157819c905826c2c7799803bebe
sedong <bulssajo00@gmail.com>
2024-11-04 19:43:45 +0900

    chore: vitest setting

---
+++ src/tests/setup.ts
@@ -0,0 +1,34 @@
+import { afterAll, afterEach, beforeAll, beforeEach } from 'vitest';
+import { cleanup, configure, prettyDOM } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+
+configure({
+  getElementError: (message: string | null, container) => {
+    const error = new Error();
+
+    if (message && message.includes('<body>')) {
+      error.message = message;
+    } else {
+      const prettifiedDOM = prettyDOM(container);
+      error.message = `${message}\n\n${prettifiedDOM}`;
+    }
+
+    error.name = 'TestingLibraryElementError';
+    return error;
+  },
+});
+
+beforeAll(() => {
+});
+
+beforeEach(() => {
+  
+});
+
+afterEach(() => {
+  cleanup();
+});
+
+afterAll(() => {
+});
