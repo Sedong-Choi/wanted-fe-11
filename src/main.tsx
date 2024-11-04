e4be66e9eb2785e559615489eded03211c865c1b
sedong <bulssajo00@gmail.com>
2024-11-04 19:25:41 +0900

    UI: update base page layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { router } from './router';
 import { RouterProvider } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline/CssBaseline';
+import { Container } from '@mui/material';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CssBaseline />
-    <RouterProvider router={router} />
+    <Container>
+      <RouterProvider router={router} />
+    </Container>
   </StrictMode>,
 )
