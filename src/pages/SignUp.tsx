5270177d979327573ddeb2b7e8610ca03f562c52
sedong <bulssajo00@gmail.com>
2024-11-04 19:30:58 +0900

    UI: Add Sign up page

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,19 @@
+import { Button, Card, CardActions, CardContent, CardHeader, Divider, TextField } from "@mui/material"
+import { FormLayout } from "./FormLayout"
+
+export const SignUpPage = () => <FormLayout>
+    <Card sx={{ width: 'min(300px,100vw)' }}>
+        <CardHeader title="Sign Up" />
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: '30px' }} >
+            <TextField id="user-id" label="Id" />
+            <TextField id="user-password" label="Password" />
+            <TextField id="user-password-confirm" label="Password Confirm" />
+        </CardContent>
+        <Divider />
+        <CardActions sx={{ display: "flex", flexDirection: 'row', padding: '16px' }}>
+            <Button variant="contained" sx={{ flex: 1 }}>
+                Sign Up
+            </Button>
+        </CardActions>
+    </Card>
+</FormLayout>
