f1a2cd28cdbb6d3a96ea62bdf02760c7ba4ab084
sedong <bulssajo00@gmail.com>
2024-11-04 19:26:51 +0900

    UI: update login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,21 @@
-export const LoginPage = () => {
-    return <>
-        <h1> Login page</h1>
-    </>
-}
+import { Button, Card, CardActions, CardContent, CardHeader, Divider, TextField } from "@mui/material"
+import { FormLayout } from "./FormLayout"
+
+export const LoginPage = () => <FormLayout>
+    <Card sx={{ width: 'min(300px,100vw)' }}>
+        <CardHeader title="Login Todo" />
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: '30px' }} >
+            <TextField id="user-id" label="Id" />
+            <TextField id="user-password" label="Password" />
+        </CardContent>
+        <Divider />
+        <CardActions sx={{ display: "flex", flexDirection: 'row', padding: '16px' }}>
+            <Button variant="contained" sx={{ flex: 1 }}>
+                Sign Up
+            </Button>
+            <Button variant="contained" sx={{ flex: 1 }}>
+                Login
+            </Button>
+        </CardActions>
+    </Card>
+</FormLayout>
