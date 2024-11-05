cee65b652897f22b8d87e397f1a574716aefb892
sedong <bulssajo00@gmail.com>
2024-11-05 14:30:52 +0900

    feat: Add global snackbar

---
+++ src/providers/SnackbarProvider.tsx
@@ -0,0 +1,53 @@
+import { PropsWithChildren } from 'react';
+import { createContext, useContext, useState } from "react"
+import Snackbar from '@mui/material/Snackbar';
+import React from 'react';
+type SnackbarData = {
+    open: () => void;
+    close?: () => void;
+    setMessage: (message: string) => void;
+    setDuration: (duration: number) => void;
+}
+const initSnackbarData = {
+    open: () => { },
+    setMessage: () => { },
+    setDuration: () => { },
+    close: () => { }
+}
+
+const SnackBarContext = createContext<SnackbarData>(initSnackbarData);
+
+export const SnakbarProvider = ({ children }: PropsWithChildren) => {
+    const [open, setOpen] = useState<boolean>(false);
+    const [message, setMessage] = useState<string>('');
+    const [duration, setDuration] = useState<number>(3000);
+
+
+
+    return <SnackBarContext.Provider value={{
+        open: () => setOpen(true),
+        close: () => setOpen(false),
+        setMessage: (message: string) => setMessage(message),
+        setDuration: (duration: number) => setDuration(duration)
+    }}>
+        {children}
+        <Snackbar
+            open={open}
+            autoHideDuration={duration}
+            onClose={() => setOpen(false)}
+            message={message}
+        // action={action}
+        />
+    </SnackBarContext.Provider>
+}
+
+
+
+export const useSnackbar = () => {
+    const context = useContext(SnackBarContext);
+    if (!context) {
+        const error = new Error(`[SnackBarContext]: Provider not found.`);
+        throw error;
+    }
+    return context
+} 
