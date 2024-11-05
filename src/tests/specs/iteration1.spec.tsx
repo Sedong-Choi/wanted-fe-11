4f4c62a6d2ad8db597bd8c6c8f83a3dbebd478b6
sedong <bulssajo00@gmail.com>
2024-11-05 22:05:46 +0900

    test: Add 'Sign up with server'

--- src/tests/specs/iteration1.spec.tsx
+++ src/tests/specs/iteration1.spec.tsx
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import { describe, test, expect } from 'vitest';
+import { renderHook, screen } from '@testing-library/react';
+import { describe, test, expect, beforeEach } from 'vitest';
 import { renderWithProviders } from 'tests/renderWithProviders';
 import { userEvent } from '@testing-library/user-event';
-import { SignUpPage } from 'pages/SignUp';
+
+
+import { mockServer } from 'tests/mocks/mockServer';
+import { mockAPI } from 'tests/mocks/mockAPI';
+import { useQueryClient } from '@tanstack/react-query';
+import { TestProviders } from 'tests/TestProviders';
+
+
+
 
 describe('init test', () => {
 
@@ -46,12 +54,17 @@ describe('routing test', () => {
 
 
 describe('Sign Up Form validate', () => {
+    beforeEach(async () => {
+        renderWithProviders();
+        const user = userEvent.setup();
+        const signUpBotton = await screen.findByText(/Sign Up page/);
+        await user.click(signUpBotton);
+
+        await screen.findByText(/Sign Up Todo/);
+    });
     // just render component
     // Providers does not necessary on Sign up form test
     test('email validate', async () => {
-        render(<SignUpPage />);
-
-        await screen.findByText(/Sign Up Todo/);
 
         const idInput = screen.getByRole('textbox', { name: 'Id' });
         await userEvent.type(idInput, 't');
@@ -64,9 +77,6 @@ describe('Sign Up Form validate', () => {
     });
 
     test('password validate', async () => {
-        render(<SignUpPage />);
-
-        await screen.findByText(/Sign Up Todo/);
 
         const passwordInput = screen.getByLabelText('Password');
         await userEvent.type(passwordInput, '1234');
@@ -79,9 +89,6 @@ describe('Sign Up Form validate', () => {
     });
 
     test('password confirm validate', async () => {
-        render(<SignUpPage />);
-
-        await screen.findByText(/Sign Up Todo/);
 
         const passwordInput = screen.getByLabelText('Password');
         await userEvent.type(passwordInput, '1234');
@@ -109,7 +116,6 @@ describe('Sign Up Form validate', () => {
     });
 
     test('Change "Sign Up" disabled', async () => {
-        render(<SignUpPage />);
 
         await screen.findByText(/Sign Up Todo/);
 
@@ -130,5 +136,59 @@ describe('Sign Up Form validate', () => {
         expect(confirmError).toBeNull();
 
         expect(singupButton).not.toBeDisabled();
+    });
+});
+
+// TODO mock server 생성하는 방식으로 변경 필요
+
+describe('Sign up with server', () => {
+    mockServer.use(
+        mockAPI.post('http://localhost:8080/users/create', {
+            token: 'test_token', message: '계정이 성공적으로 생성되었습니다'
+        })
+    );
+
+    test('navigate to "/auth/signup",create user "test@test.ts" and redirect', async () => {
+        // renderWithProviders 사용안함
+        const { result } = renderHook(() => useQueryClient(), { wrapper: TestProviders });
+
+
+        const signUpBotton = await screen.findByText(/Sign Up page/);
+
+        const user = userEvent.setup();
+
+        await user.click(signUpBotton);
+        expect(screen.getByText(/Sign Up Todo/)).toBeInTheDocument();
+
+        const idInput = screen.getByRole('textbox', { name: 'Id' });
+        await userEvent.type(idInput, 'test@test.ts');
+        const emailError = screen.queryByText(/Please enter email/);
+        expect(emailError).toBeNull();
+
+        const passwordInput = screen.getByLabelText('Password');
+        await userEvent.type(passwordInput, '12341234');
+        const passwordError = screen.queryByText(/Password must more than 8 charactor/);
+        expect(passwordError).toBeNull();
+
+        const confirmInput = screen.getByLabelText(/Password Confirm/);
+        await userEvent.type(confirmInput, '12341234');
+        const confirmError = screen.queryByText(/Passwords do not match/);
+        expect(confirmError).toBeNull();
+
+        const singupButton = screen.getByRole('button', { name: "Sign Up" });
+        expect(singupButton).not.toBeDisabled();
+
+        await user.click(singupButton);
+
+        const token = result.current.getQueryData(['userToken']);
+        expect(token).not.toBeNull();
+
+
+        await screen.findByText(/계정이 성공적으로 생성되었습니다/);
+        expect(screen.getByText(/계정이 성공적으로 생성되었습니다/)).toBeInTheDocument();
+
+
+        await screen.findByText(/App main/);
+        expect(screen.getByText(/App main/)).toBeInTheDocument();
     });
 });
