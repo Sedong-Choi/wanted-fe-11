8fa4f19186b9ab66b7e54aa2510e3845044d6f44
sedong <bulssajo00@gmail.com>
2024-11-05 12:22:36 +0900

    test: Add sign up form validate
    - email validte
    - password validte
    - confirm password validate
    - signup button disable change

--- src/tests/specs/iteration1.spec.tsx
+++ src/tests/specs/iteration1.spec.tsx
@@ -1,7 +1,8 @@
-import {  screen } from '@testing-library/react';
-import { describe, test, expect} from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, test, expect } from 'vitest';
 import { renderWithProviders } from 'tests/renderWithProviders';
 import { userEvent } from '@testing-library/user-event';
+import { SignUpPage } from 'pages/SignUp';
 
 describe('init test', () => {
 
@@ -41,4 +42,93 @@ describe('routing test', () => {
         await user.click(signUpBotton);
         expect(screen.getByText(/Sign Up Todo/)).toBeInTheDocument();
     });
-})
+});
+
+
+describe('Sign Up Form validate', () => {
+    // just render component
+    // Providers does not necessary on Sign up form test
+    test('email validate', async () => {
+        render(<SignUpPage />);
+
+        await screen.findByText(/Sign Up Todo/);
+
+        const idInput = screen.getByRole('textbox', { name: 'Id' });
+        await userEvent.type(idInput, 't');
+        await screen.findByText('Please enter email');
+        expect(screen.getByText(/Please enter email/)).toBeInTheDocument();
+
+        await userEvent.type(idInput, 'test@test.ts');
+        const emailError = screen.queryByText(/Please enter email/);
+        expect(emailError).toBeNull();
+    });
+
+    test('password validate', async () => {
+        render(<SignUpPage />);
+
+        await screen.findByText(/Sign Up Todo/);
+
+        const passwordInput = screen.getByLabelText('Password');
+        await userEvent.type(passwordInput, '1234');
+        await screen.findByText(/Password must more than 8 charactor/);
+        expect(screen.getByText(/Password must more than 8 charactor/)).toBeInTheDocument();
+
+        await userEvent.type(passwordInput, '12341234');
+        const passwordError = screen.queryByText(/Password must more than 8 charactor/);
+        expect(passwordError).toBeNull();
+    });
+
+    test('password confirm validate', async () => {
+        render(<SignUpPage />);
+
+        await screen.findByText(/Sign Up Todo/);
+
+        const passwordInput = screen.getByLabelText('Password');
+        await userEvent.type(passwordInput, '1234');
+        await screen.findByText(/Password must more than 8 charactor/);
+        expect(screen.getByText(/Password must more than 8 charactor/)).toBeInTheDocument();
+
+        await userEvent.type(passwordInput, '1234');
+        const passwordError = screen.queryByText(/Password must more than 8 charactor/);
+        expect(passwordError).toBeNull();
+
+        const confirmInput: HTMLInputElement = screen.getByLabelText(/Password Confirm/);
+        await userEvent.type(confirmInput, '1234');
+        await screen.findByText(/Passwords do not match/);
+        expect(screen.getByText(/Passwords do not match/)).toBeInTheDocument();
+
+
+        await userEvent.type(confirmInput, '12345');
+        await screen.findByText(/Passwords do not match/);
+        expect(screen.getByText(/Passwords do not match/)).toBeInTheDocument();
+
+        confirmInput.setSelectionRange(7, 8)
+        await userEvent.type(confirmInput, '{backspace}');
+        const confirmError = screen.queryByText(/Passwords do not match/);
+        expect(confirmError).toBeNull();
+    });
+
+    test('Change "Sign Up" disabled', async () => {
+        render(<SignUpPage />);
+
+        await screen.findByText(/Sign Up Todo/);
+
+        const singupButton = screen.getByRole('button', { name: "Sign Up" });
+        expect(singupButton).toBeDisabled();
+
+        const idInput = screen.getByLabelText('Id');
+        await userEvent.type(idInput, 'test@test.ts');
+
+        const passwordInput = screen.getByLabelText('Password');
+        await userEvent.type(passwordInput, 'testpass');
+        const passwordError = screen.queryByText(/Password must more than 8 charactor/);
+        expect(passwordError).toBeNull();
+
+        const confirmInput: HTMLInputElement = screen.getByLabelText(/Password Confirm/);
+        await userEvent.type(confirmInput, 'testpass');
+        const confirmError = screen.queryByText(/Passwords do not match/);
+        expect(confirmError).toBeNull();
+
+        expect(singupButton).not.toBeDisabled();
+    });
+});
