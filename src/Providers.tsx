2444c0cd0550a836d11cd7bf53be6dbd71e131c4
sedong <bulssajo00@gmail.com>
2024-11-05 14:38:11 +0900

    feat: Add Global Snackbar

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,6 +1,9 @@
 import { QueryProvider } from './providers/QueryProvider';
+import { SnackbarProvider } from './providers/SnackbarProvider';
 export const Providers = ({ children }) => {
     return <QueryProvider>
-        {children}
+        <SnackbarProvider>
+            {children}
+        </SnackbarProvider>
     </QueryProvider>
 }
