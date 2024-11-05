417d90f23f9505c9e6eeea09d6c1cc9bc7387431
sedong <bulssajo00@gmail.com>
2024-11-05 16:47:38 +0900

    feat: update sign up with server

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,10 +1,17 @@
 import { Button, Card, CardActions, CardContent, CardHeader, Divider, TextField, Typography } from "@mui/material"
 import { FormLayout } from "./FormLayout"
 import { useSignUpForm } from "hooks/useSignUpForm";
-import { useCallback } from "react";
+import { useSnackbar } from "providers/SnackbarProvider";
+import { useNavigate } from "react-router";
+import { useQueryClient } from "@tanstack/react-query";
 
 export const SignUpPage = () => {
 
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
+    
+    const { open, setMessage } = useSnackbar();
+    
     const {
         id,
         setId,
@@ -18,56 +25,66 @@ export const SignUpPage = () => {
         disabled
     } = useSignUpForm();
 
-    const handleSignup = useCallback(async () => {
-        if (!disabled) {
-            const response = await fetch('http://localhost:8080/users/create', {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email: id,
-                    password: password // todo encrypt
-                }),
+    const handleSignup = async (e) => {
+        e.preventDefault();
+        const response = await fetch('http://localhost:8080/users/create', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: id, password }),
+        });
 
-            });
-
+        if (response.status === 409) {
             const result = await response.json();
-            if (result.message === '계정이 성공적으로 생성되었습니다') {
-                // TODO use tanstack query persist
-                window.localStorage.setItem(`${id}_token`, result.token);
-                // TODO open global snakbar and redirect main page
-            }
+            setMessage(result.details);
+            open();
+            return;
         }
-    }, [disabled, id, password]);
+
+        if (!response.ok) {
+            setMessage("서버 오류");
+            open();
+            return;
+        }
+
+        const result = await response.json();
+        queryClient.setQueryData(['userToken'], result.token);
+        setMessage(result.message);
+        open();
+        navigate('/');
+    };
 
 
     return (<FormLayout>
-        <Card sx={{ width: 'min(300px,100vw)' }}>
-            <CardHeader title="Sign Up Todo" />
-            <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: '30px' }} >
-                <TextField id="user-id" label="Id" value={id} onChange={(e) => setId(e.target.value)} />
-                {
-                    errorId !== ''
-                    && <Typography color="red">{errorId}</Typography>
-                }
-                <TextField id="user-password" label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                {
-                    errorPassword !== ''
-                    && <Typography color="red">{errorPassword}</Typography>
-                }
-                <TextField id="user-password-confirm" label="Password Confirm" type="password" value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} />
-                {
-                    errorConfirm !== ''
-                    && <Typography color="red">{errorConfirm}</Typography>
-                }
-            </CardContent>
-            <Divider />
-            <CardActions sx={{ display: "flex", flexDirection: 'row', padding: '16px' }}>
-                <Button variant="contained" sx={{ flex: 1 }} disabled={disabled} onClick={handleSignup}>
-                    Sign Up
-                </Button>
-            </CardActions>
-        </Card>
+        <form onSubmit={handleSignup}>
+            <Card sx={{ width: 'min(300px,100vw)' }}>
+                <CardHeader title="Sign Up Todo" />
+                <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: '30px' }} >
+
+                    <TextField id="user-id" label="Id" value={id} onChange={(e) => setId(e.target.value)} />
+                    {
+                        errorId !== ''
+                        && <Typography color="red">{errorId}</Typography>
+                    }
+                    <TextField id="user-password" label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    {
+                        errorPassword !== ''
+                        && <Typography color="red">{errorPassword}</Typography>
+                    }
+                    <TextField id="user-password-confirm" label="Password Confirm" type="password" value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} />
+                    {
+                        errorConfirm !== ''
+                        && <Typography color="red">{errorConfirm}</Typography>
+                    }
+                </CardContent>
+                <Divider />
+                <CardActions sx={{ display: "flex", flexDirection: 'row', padding: '16px' }}>
+                    <Button variant="contained" sx={{ flex: 1 }} disabled={disabled} type="submit">
+                        Sign Up
+                    </Button>
+                </CardActions>
+            </Card>
+        </form>
     </FormLayout>)
 }
