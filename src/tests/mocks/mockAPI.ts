b5bb8c0e6e367d4cfaef1e028b47544b4d4d8718
sedong <bulssajo00@gmail.com>
2024-11-08 15:18:42 +0900

    test: update setting
    - add mockAPI.get parameter - status

--- src/tests/mocks/mockAPI.ts
+++ src/tests/mocks/mockAPI.ts
@@ -1,8 +1,8 @@
 import { http, HttpResponse } from 'msw';
 export const mockAPI = {
-  get: (path: string, data: object) =>
+  get: (path: string, data: object,status?:number) =>
     http.get(path, () => {
-      return HttpResponse.json(data, { status: 200 });
+      return HttpResponse.json(data, { status: status ?? 200 });
     }),
   post: (path: string, data: object) =>
     http.post(path, () => {
